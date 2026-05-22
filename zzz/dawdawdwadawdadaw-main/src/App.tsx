45cce7b580c3f2d3caf97bd76447bfaa014c9c5e
lukasgamer339 <130569069+lukasgamer339@users.noreply.github.com>
2026-05-22 15:34:46 +0200

    Delete zzz/dawdawdwadawdadaw-main directory

--- zzz/dawdawdwadawdadaw-main/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function App() {
-  return (
-    <div className="p-8 text-neutral-400 font-sans text-center">
-      Loaded via pure HTML entrypoint instead. See main index.html file.
-    </div>
-  );
-}
